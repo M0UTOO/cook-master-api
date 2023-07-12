1988d370080a218b7d019c509f303f17ea07c424
M0UTOO <ninoplane@gmail.com>
2023-07-12 02:47:31 -0700

    fix on utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -7,7 +7,7 @@ import (
 
 func IsSafeString(input string) bool {
 
-	injectionPattern := `(?i)(\bSELECT\b|\bINSERT\b|\bUPDATE\b|\bDELETE\b|\bDROP\b|\bUNION\b|\bEXEC\b|\bALTER\b|\bTRUNCATE\b|\bOR\b|\bAND\b|\b;|\b--\s)`
+	injectionPattern := `(?i)(\bSELECT\b|\bINSERT\b|\bUPDATE\b|\bDELETE\b|\bDROP\b|\bUNION\b|\bEXEC\b|\bALTER\b|\bTRUNCATE\b|\b;|\b--\s)`
 	regex := regexp.MustCompile(injectionPattern)
 	match := regex.MatchString(input)
 
